88a64aa136a2504c8d35b1884ce4848a3c9865cc
rgcloud <ganeshsolankar2008@gmail.com>
2020-02-15 14:44:29 +0530

    v0.1

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+<title> ..:::: Welcome To SGcloud ::::.. </title>
+  
+  
+<center> <h1> Welcome To Our Openshift DevOps(CI/CD) Project</h1> </center>
+
+<?php
+
+echo "<center><h2>How are you getting on :-)   </h2></center>";
+  
+
+?>
+  
+  <center><h2><p><font color="green">Production v2.0 Green Deployment!</font></p></h2></center>
+
+  
+</body>
+</html>
+© 2020 GitHub, Inc.
